0f9a44f1dd0d78b64519bcad223087b1ae09a21e
vvede <v.vedeshenkov@gmail.com>
2022-08-20 18:08:42 -0700

    Apply button was added

--- src/day3/RegisterFeature.java
+++ src/day3/RegisterFeature.java
@@ -4,5 +4,6 @@ public class RegisterFeature {
     public static void main(String[] args) {
         System.out.println("Register button");
         System.out.println("User info");
+        System.out.println("Apply");
     }
 }
