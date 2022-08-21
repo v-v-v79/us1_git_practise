7b3329e6945623e50fd945720cf89696a4ecf361
vvede <v.vedeshenkov@gmail.com>
2022-08-20 17:31:41 -0700

    Password tab was added

--- src/day2/LoginPage.java
+++ src/day2/LoginPage.java
@@ -4,5 +4,6 @@ public class LoginPage {
     public static void main(String[] args) {
         System.out.println("Login page");
         System.out.println("Login tab");
+        System.out.println("Password tab");
     }
 }
