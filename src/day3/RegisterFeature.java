094ef7ad25307fe0784fff922ebb1b30cc212dc2
vvede <v.vedeshenkov@gmail.com>
2022-08-20 18:07:34 -0700

    User info was added

--- src/day3/RegisterFeature.java
+++ src/day3/RegisterFeature.java
@@ -3,5 +3,6 @@ package day3;
 public class RegisterFeature {
     public static void main(String[] args) {
         System.out.println("Register button");
+        System.out.println("User info");
     }
 }
